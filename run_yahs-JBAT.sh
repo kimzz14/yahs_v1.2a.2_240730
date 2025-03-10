bd686dac455faf6ede013c0a99f293f51f8bfb25
kimzz14 <kimzz1004@gmail.com>
2025-03-10 10:42:10 +0900

    rename

--- run_yahs-JBAT.sh
+++ run_yahs-JBAT.sh
@@ -1,5 +1,10 @@
-juicer pre -a -o yahs-JBAT/yahs_JBAT yahs-scaffolding/yahs.bin yahs-scaffolding/yahs_scaffolds_final.agp ref/ref.fa.fai > yahs-JBAT/yahs_JBAT.log 2>&1
+juicer pre -a \
+    -o result/yahs_JBAT \
+       result/yahs.bin \
+       result/yahs_scaffolds_final.agp \
+       ref/ref.fa.fai \
+       > result/yahs_JBAT.log 2>&1
 
-cat yahs-JBAT/yahs_JBAT.log | grep "PRE_C_SIZE" | cut -d' ' -f2- > yahs-JBAT/yahs_JBAT.chrom.sizes
+cat result/yahs_JBAT.log | grep "PRE_C_SIZE" | cut -d' ' -f2- > result/yahs_JBAT.chrom.sizes
 
-java -jar -Xmx256G ./script/juicer_tools.1.9.9_jcuda.0.8.jar pre yahs-JBAT/yahs_JBAT.txt yahs-JBAT/yahs_JBAT.hic yahs-JBAT/yahs_JBAT.chrom.sizes > run_yahs-JBAT.log 2>&1
+java -jar -Xmx256G ./script/juicer_tools.1.9.9_jcuda.0.8.jar pre result/yahs_JBAT.txt result/yahs_JBAT.hic result/yahs_JBAT.chrom.sizes > run_yahs-JBAT.log 2>&1
--- run_yahs-scaffolding.sh
+++ run_yahs-scaffolding.sh
@@ -1,5 +1,5 @@
 yahs \
-    -o yahs-scaffolding/yahs \
+    -o result/yahs \
     --no-mem-check \
     ref/ref.fa \
     aligned.bam \
@@ -9,4 +9,4 @@ yahs \
 #-r 2000,10000,20000,50000,100000,200000,500000,1000000,2000000,5000000,10000000,20000000,50000000,100000000,200000000,500000000 \
 #--no-contig-ec \
 
-samtools faidx yahs-scaffolding/yahs_scaffolds_final.fa
+samtools faidx result/yahs_scaffolds_final.fa
